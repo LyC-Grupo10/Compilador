f443253f24596790787c7580f14f1cec120eea05
ashfirpo <ayelenfirpo@gmail.com>
2020-07-01 21:34:23 -0300

    Revert "Merge branch 'master' of https://github.com/LyC-Grupo10/Grupo10"
    This reverts commit fb8d71e5d4d0cea8f2eb7bb64cb934cd26e795e5, reversing
    changes made to 54f71963358b3d8fc0c6c6e119d69a0dd8a9702c.

--- Entrega Final/Final.asm
+++ Entrega Final/Final.asm
@@ -13,27 +13,18 @@ b              dd             ?              ; Variable int
 c              dd             ?              ; Variable int 
 i              dd             ?              ; Variable int 
 x              dd             ?              ; Variable int 
+f              dd             ?              ; Variable float
 z              db             ?              ; Variable string
+_4             dd             4.0            ; Constante int
 _5             dd             5.0            ; Constante int
-_10            dd             10.0           ; Constante int
-_30             dd             30.0            ; Constante int
-_else          db             "else", '$', 4 dup (?); Constante string
-_7             dd             7.0            ; Constante int
-_1             dd             1.0            ; Constante int
-_menor         db             "menor", '$', 5 dup (?); Constante string
-_mayor         db             "mayor", '$', 5 dup (?); Constante string
 _2             dd             2.0            ; Constante int
-_Test          db             "Test", '$', 4 dup (?); Constante string
-@var           dd             ?              ; Variable int 
-@aux           dd             ?              ; Variable int 
-@res           dd             ?              ; Variable int 
-@varN          dd             ?              ; Variable int 
-@resN          dd             ?              ; Variable int 
-@varM          dd             ?              ; Variable int 
-@resM          dd             ?              ; Variable int 
-@varNM         dd             ?              ; Variable int 
-@resNM         dd             ?              ; Variable int 
-_@calc  dd  ?
+_1             dd             1.0            ; Constante int
+_3             dd             3.0            ; Constante int
+S_otro_if_en_la_parte_true_1  db             "otro if en la parte true", '$', 24 dup (?); Constante string
+_10            dd             10.0           ; Constante int
+S_parte_else_del_if_2         db             "parte else del if", '$', 17 dup (?); Constante string
+_4_2           dd             4.2            ; Constante float
+S_between_true_3              db             "between true", '$', 12 dup (?); Constante string
 
 .CODE
 
@@ -43,39 +34,45 @@ mov AX,@DATA                  ; Inicializa el segmento de datos
 mov DS,AX                     
 mov ES,AX                     
 
-fld _1
-fstp a
-fld _1
+fld _4
 fstp b
 
-ffree
-
-; empieza mi comparacion
-;parte izquierda:
-; 2*a+30
-
-fld _2
-fld a
-fmul
-fld _30
-fadd
-
-fld _2
-fld a
-fmul
-fld _30
-fadd
 fld _5
-fld a
+fstp c
+
 fld b
-fadd
+fld _2
 fmul
 fld _1
-fsub
-fstp @res
-displayFloat @res, 2
+fadd
+fld b
+fld c
+fadd
+fld _3
+fdiv
+displayString S_otro_if_en_la_parte_true_1
 
-mov AX,4C00h                  ; Indica que debe finalizar la ejecuci?n
+fld c
+fld _10
+fld _2
+fstp a
+
+fld _1
+fstp a
+
+displayString S_parte_else_del_if_2
+
+fld b
+fld _1
+fld b
+fld a
+fld _10
+fmul
+fld _4_2
+fsub
+displayString S_between_true_3
+
+mov AX,4C00h                  ; Indica que debe finalizar la ejecución
 int 21h
 
 END inicio
